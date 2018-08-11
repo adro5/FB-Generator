2877bc8a1998d589024b017ac9bc9dcaf938546c
Adam Robinson <adro5@users.noreply.github.com>
2018-08-10 22:31:07 -0400

    Refactor and make tiny increase efficiency
    Still working with timers (disregard timers)

--- fbgenerator.py
+++ fbgenerator.py
@@ -9,38 +9,54 @@ Next step: Organize and comment and fix slow loop
 
 import pandas as pd
 import tkinter as tk
+from tkinter import filedialog
 import urllib.request
 import urllib.parse
 import re
-from tkinter import filedialog
+import time
 
-root = tk.Tk()
-root.withdraw()
+def merge():
+    root = tk.Tk()
+    root.withdraw()
 
-year1 = pd.read_csv(filedialog.askopenfilename(filetypes=(("csv UTF-8 files", "*.csv"),)))
-year2 = pd.read_csv(filedialog.askopenfilename(filetypes=(("csv UTF-8 files", "*.csv"),)))
-merged_tbl = year1.merge(year2, how="outer")
+    year1 = pd.read_csv(filedialog.askopenfilename(filetypes=(("csv UTF-8 files", "*.csv"),)))
+    year2 = pd.read_csv(filedialog.askopenfilename(filetypes=(("csv UTF-8 files", "*.csv"),)))
+    merged_tbl = year1.merge(year2, how="outer")
 
-merged_tbl.to_csv("merged_data.csv", index=False)
-merged_tbl = merged_tbl.dropna(axis=1)
+    merged_tbl.to_csv("merged_data.csv", index=False)
+    return
 
-selected_data = merged_tbl.loc[0:, 'Date':'Opp']
+def generate():
+    merged_tbl= pd.read_csv("merged_data.csv")
+    merged_tbl = merged_tbl.dropna(axis=1)
 
-selected_data['Score Diff'] = pd.Series(selected_data['Pts'] - selected_data['Opp'], index=selected_data.index)
-selected_data['Total Score'] = pd.Series(selected_data['Pts'] + selected_data['Opp'], index=selected_data.index)
-selected_data['RVal'] = pd.Series((2*selected_data['Score Diff']) + (selected_data['Total Score']/(2*selected_data['Score Diff'])), index=selected_data.index)
+    selected_data = merged_tbl.loc[0:, 'Date':'Opp']
+
+    selected_data['Score Diff'] = pd.Series(selected_data['Pts'] - selected_data['Opp'], index=selected_data.index)
+    selected_data['Total Score'] = pd.Series(selected_data['Pts'] + selected_data['Opp'], index=selected_data.index)
+    selected_data['RVal'] = pd.Series((2*selected_data['Score Diff']) + (selected_data['Total Score']/(2*selected_data['Score Diff'])), index=selected_data.index)
     
-selected_data.sort_values(['RVal','Opponent'],kind='mergesort',inplace=True)
+    selected_data.sort_values(['RVal','Opponent'],kind='mergesort',inplace=True)
 
-print(selected_data)
-selected_data.to_csv("final_rank.csv", index=False)
+    print(selected_data)
+    selected_data.to_csv("final_rank.csv", index=False)
 
-for i, row in selected_data.iterrows():
-    strFBq = row[0] + " " + row[2] + " " + row[3]
-    query_string = urllib.parse.urlencode({"search_query" : strFBq})
-    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    print("http://www.youtube.com/watch?v=" + search_results[0])
+    start = time.time()
+    for i, row in selected_data.iterrows():
+        strFBq = row[0] + " " + row[2] + " " + row[3]
+        query_string = urllib.parse.urlencode({"search_query" : strFBq})
+        html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+        search_results = re.search(r'href=\"\/watch\?v=(.{11})', html_content.read().decode()).group()
+        print("http://www.youtube.com/watch?v=" + search_results)
+        
+    end = time.time()
+    print(end-start)
+    return
+
+merge()
+generate()
+
+
 
 
 
